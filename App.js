3fe500784b675e5ffae00bc49bbf7232b9c7fcd4
Phillip Doran <wheninbrome@bitbucket.org>
2019-12-04 16:10:20 -0500

    firebase, alias setup

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
+import React from "react"
+
+// Navigators
 import { createAppContainer, createSwitchNavigator } from "react-navigation"
 import { createStackNavigator } from "react-navigation-stack"
 import { createBottomTabNavigator } from "react-navigation-tabs"
 
-import LoginScreen from "./src/screens/loginScreen"
-import SignupScreen from "./src/screens/signupScreen"
-import HomeScreen from "./src/screens/homeScreen"
-import ProfileScreen from "./src/screens/profileScreen"
-import CreateScreen from "./src/screens/createScreen"
+// Screens
+import LoginScreen from "@screens/loginScreen"
+import SignupScreen from "@screens/signupScreen"
+import HomeScreen from "@screens/homeScreen"
+import ProfileScreen from "@screens/profileScreen"
+import CreateScreen from "@screens/createScreen"
+
+// Services
+import { initializeFirebase } from "@services/firebase"
 
 const switchNavigator = createSwitchNavigator({
 	authFlow: createStackNavigator({
@@ -20,4 +27,12 @@ const switchNavigator = createSwitchNavigator({
 	})
 })
 
-export default createAppContainer(switchNavigator)
+const AppContainer = createAppContainer(switchNavigator)
+
+const App = () => {
+	initializeFirebase()
+  
+	return <AppContainer />
+}
+
+export default App
