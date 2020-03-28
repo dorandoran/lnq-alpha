6b0d92dd2ca5190ecdec9e1474d46b05f3f05c3d
Phillip Doran <phillip.doran@camelcasestudios.io>
2020-03-28 08:40:20 -0400

    fix file structure

--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -3,7 +3,7 @@ import { auth } from '@services/firebase'
 import * as Google from 'expo-google-app-auth'
 import * as Facebook from 'expo-facebook'
 import { f } from '@services/firebase'
-import useCreateUser from '@auth/useCreateUser'
+import useCreateUser from '@components/auth/useCreateUser'
 
 const AuthContext = createContext()
 
