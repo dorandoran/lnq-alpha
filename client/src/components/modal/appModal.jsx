03b5c77cc3972ba4a04fa20ab468e3f8ce52be00
Phillip Doran <doran.phillip.3ed@gmail.com>
2020-04-11 12:47:41 -0400

    change file import

--- client/src/components/modal/appModal.jsx
+++ client/src/components/modal/appModal.jsx
@@ -11,12 +11,16 @@ import {
   StyleSheet,
   Modal,
   ImageBackground,
-  TouchableOpacity
+  TouchableOpacity,
+  ScrollView
 } from 'react-native'
 import { Icon } from 'react-native-elements'
+import Constants from 'expo-constants'
 
-import ViewContainer from '@components/main/viewContainer'
 import { Header } from '@common'
+import Loading from '@common/loading'
+import { SCREEN_HEIGHT, SCREEN_WIDTH } from '@common/constants'
+import ViewContainer from '@components/main/viewContainer'
 
 const AppModal = () => {
   const { modal, objectId } = useContext(Route.State)
@@ -27,59 +31,92 @@ const AppModal = () => {
     skip: !objectId
   })
 
+  if (loading) {
+    return <Loading />
+  }
+
   return (
     <Modal visible={modal} statusBarTranslucent>
       <ViewContainer>
-        {loading ? (
-          <React.Fragment>
-            <Text style={styles.text}>App Modal</Text>
-            <Text style={styles.text}>App Modal</Text>
-            <Text style={styles.text}>App Modal</Text>
-            <Text style={styles.text}>App Modal</Text>
-            <Text style={styles.text}>App Modal</Text>
-            <Text style={styles.text}>App Modal</Text>
-            <Text style={styles.text}>App Modal</Text>
-            <Text style={styles.text}>App Modal</Text>
-          </React.Fragment>
-        ) : (
+        <ScrollView
+          style={[styles.container, styles.flex]}
+          snapToInterval={SCREEN_HEIGHT - Constants.statusBarHeight}
+        >
           <ImageBackground
             source={{ uri: data?.event.media[0].uri }}
             style={styles.image}
           >
-            <Header>
-              <TouchableOpacity
-                onPress={() => dispatch({ type: 'closeModal' })}
-                style={styles.iconContainer}
-              >
-                <Icon
-                  type="ionicon"
-                  name="ios-arrow-back"
-                  color={theme.color.tertiary}
-                />
-              </TouchableOpacity>
-              <View />
-              <View />
-            </Header>
+            <View style={styles.imageContainer}>
+              <Header position="relative">
+                <TouchableOpacity
+                  onPress={() => dispatch({ type: 'closeModal' })}
+                  style={styles.iconContainer}
+                >
+                  <Icon
+                    type="ionicon"
+                    name="ios-arrow-back"
+                    color={theme.color.tertiary}
+                  />
+                </TouchableOpacity>
+                <View />
+                <TouchableOpacity
+                  onPress={() => {}}
+                  style={styles.iconContainer}
+                >
+                  <Icon
+                    type="material-community"
+                    name="menu"
+                    color={theme.color.tertiary}
+                  />
+                </TouchableOpacity>
+              </Header>
+              <Header position="relative">
+                <View />
+                <View />
+                <TouchableOpacity
+                  onPress={() => {}}
+                  style={styles.iconContainer}
+                >
+                  <Icon
+                    type="ionicon"
+                    name="ios-arrow-up"
+                    color={theme.color.tertiary}
+                  />
+                </TouchableOpacity>
+              </Header>
+            </View>
           </ImageBackground>
-        )}
+          <View style={[styles.infoContainer, styles.image]}>
+            <Text style={styles.text}>This is text</Text>
+          </View>
+        </ScrollView>
       </ViewContainer>
     </Modal>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.color.background
+  },
+  imageContainer: {
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  infoContainer: {},
   iconContainer: {
     backgroundColor: theme.color.accent,
     aspectRatio: 1,
-    padding: 1,
+    padding: 3,
     borderRadius: 25
   },
   text: {
     color: theme.color.tertiary
   },
   image: {
-    flex: 1,
-    resizeMode: 'cover'
+    height: SCREEN_HEIGHT - 2 * Constants.statusBarHeight,
+    width: SCREEN_WIDTH
   }
 })
 
