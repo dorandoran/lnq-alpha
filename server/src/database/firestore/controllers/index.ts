a5381ee62f93771e1048e89d8202f1f59e874a8d
Phillip <doran.phillip.3ed@gmail.com>
2020-10-10 17:55:34 -0400

    fix ?

--- server/src/database/firestore/controllers/index.ts
+++
@@ -1,5 +0,0 @@
-export * as UserController from './UserController'
-export * as EventController from './EventController'
-export * as MediaController from './MediaController'
-export * as InviteController from './InviteController'
-export * as FollowController from './FollowController'
--- server/src/database/firestore/interfaces/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './User'
-export * from './Event'
-export * from './Location'
-export * from './Media'
-export * from './SocialLink'
-export * from './Search'
