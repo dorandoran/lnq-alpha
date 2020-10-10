3c5cce3aa2d52a39b4a55c8047ab9db87fe8dbd7
Phillip <doran.phillip.3ed@gmail.com>
2020-10-10 17:53:46 -0400

    fix .gitignore

--- server/build/database/firestore/interfaces/Search.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EBuckets = void 0;
-var EBuckets;
-(function (EBuckets) {
-    EBuckets["USERS"] = "users";
-    EBuckets["EVENTS"] = "events";
-})(EBuckets = exports.EBuckets || (exports.EBuckets = {}));
